033b9f09aed3a2414ee6624c43ed7eefec1164a8
Hung <huynhhung2108@gmail.com>
2023-11-07 21:06:04 +0700

    .

--- MiniWord_HuynhDucHung/frm_Hung.cs
+++ MiniWord_HuynhDucHung/frm_Hung.cs
@@ -19,16 +19,19 @@ namespace MiniWord_HuynhDucHung
         public frm_Hung()
         {
             InitializeComponent();
-            loadFont();
+            init();
         }
-
+        private void init()
+        {
+            loadFont();
+            this.FormClosing += new FormClosingEventHandler(MainForm_FormClosing);
+            rtbContent.SelectionFont = new Font("Times New Roman", 14, FontStyle.Regular);
+            
+        }
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
             isSaved = false;
         }
-
-
-
         private void loadFont()
         {
             
@@ -41,33 +44,55 @@ namespace MiniWord_HuynhDucHung
                 cbbFonts.Items.Add(fontName);
             }
         }
-
         private void cbbFonts_Click(object sender, EventArgs e)
         {
 
         }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
 
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+
+                exit();
+            }
+        }
         private void toolExit_Click(object sender, EventArgs e)
         {
-
-            if (!isSaved)
-            {
-                DialogResult result = ShowCustomMessageBox("Save to change your this file?"
-                                                            , "Notification", "Save", "Don't Save", "Cancel");
-            }
-            else
-            {
-                Application.Exit();
-            }
-            
+            exit();
 
         }
-        private DialogResult ShowCustomMessageBox(string message, string caption, string button1Text, string button2Text, string button3Text)
+        private void exit()
         {
+            bool exit = true;
+            if (!isSaved)
+            {
+
+                DialogResult result = ShowCustomMessageBoxExit();
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        save();
+                        break;
+                    case DialogResult.Cancel:
+                        exit = false;
+                        break;
+                }
+
+            }
+            if (exit)
+            {
+                Application.ExitThread();
+            }
+        }
+        private DialogResult ShowCustomMessageBoxExit()
+        {
+            DialogResult result = DialogResult.None;
             using (var form = new Form())
             {
                 form.Size = new System.Drawing.Size(300, 150);
-                form.Text = caption;
+                form.Text = "Notification";
                 form.FormBorderStyle = FormBorderStyle.FixedDialog;
                 form.StartPosition = FormStartPosition.CenterScreen;
                 form.MaximizeBox = false;
@@ -75,75 +100,79 @@ namespace MiniWord_HuynhDucHung
 
                 var label = new Label
                 {
-                    Text = message,
+                    Text = "Save to change your this file?",
                     AutoSize = true,
                     Location = new System.Drawing.Point(20, 20)
                 };
 
-                var button1 = new Button
+                var buttonSave = new Button
                 {
-                    Text = button1Text,
+                    Text = "Save",
                     DialogResult = DialogResult.Yes,
                     Location = new System.Drawing.Point(20, 80)
                 };
 
-                var button2 = new Button
+                var buttonDontSave = new Button
                 {
-                    Text = button2Text,
+                    Text = "Don't Save",
                     DialogResult = DialogResult.No,
                     Location = new System.Drawing.Point(100, 80)
                 };
 
-                var button3 = new Button
+                var buttonCancel = new Button
                 {
-                    Text = button3Text,
+                    Text = "Cancel",
                     DialogResult = DialogResult.Cancel,
                     Location = new System.Drawing.Point(180, 80)
                 };
 
-                button1.Click += (sender, e) =>
+                buttonSave.Click += (sender, e) =>
                 {
-                    save();
-                    Application.Exit();
-                }
-                ;
-                button2.Click += (sender, e) => {
-                    Application.Exit();
+                    result = DialogResult.Yes;
+                    form.Close();
+                };
+
+                buttonDontSave.Click += (sender, e) =>
+                {
+                    result = DialogResult.No;
+                    form.Close();
+                };
+
+                buttonCancel.Click += (sender, e) =>
+                {
+                    result = DialogResult.Cancel;
+                    form.Close();
                 };
 
                 form.Controls.Add(label);
-                form.Controls.Add(button1);
-                form.Controls.Add(button2);
-                form.Controls.Add(button3);
+                form.Controls.Add(buttonSave);
+                form.Controls.Add(buttonDontSave);
+                form.Controls.Add(buttonCancel);
 
-                return form.ShowDialog();
+                form.ShowDialog();
+
+                return result;
+
             }
         }
-
-
-
         private void btnSave_Click(object sender, EventArgs e)
         {
             save();
         }
-
         private void toolSave_Click(object sender, EventArgs e)
         {
             save();
         }
-
         private void toolSaveAs_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+            saveFileDialog.Filter = "Rich Text Files|.rtb|All Files|.*"; // Change filter to *.rtb
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 filePath = saveFileDialog.FileName;
                 saveContentToFile(filePath);
             }
         }
-
-
         private void save()
         {
             if (!string.IsNullOrEmpty(filePath))
@@ -153,7 +182,7 @@ namespace MiniWord_HuynhDucHung
             else
             {
                 SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+                saveFileDialog.Filter = "Rich Text Files|.rtb|All Files|.*"; // Change filter to *.rtb
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     filePath = saveFileDialog.FileName;
@@ -161,52 +190,61 @@ namespace MiniWord_HuynhDucHung
                 }
             }
         }
-
         private void saveContentToFile(string filePath)
         {
-            rtbContent.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+            rtbContent.SaveFile(filePath, RichTextBoxStreamType.RichText); // Use RichText instead of PlainText
             isSaved = true;
         }
-
         private void toolOpenFile_Click(object sender, EventArgs e)
         {
             openFile();
         }
-
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
             openFile();
         }
-
-
         private void openFile()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+            openFileDialog.Filter = "Rich Text Files|*.rtb|All Files|*.*";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
+                rtbContent.Clear();
                 filePath = openFileDialog.FileName;
-                rtbContent.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+                rtbContent.LoadFile(filePath, RichTextBoxStreamType.RichText); // Use RichTextStreamType.RichText
+                isSaved = true;
             }
         }
-
         private void cbbFontSizes_SelectedIndexChanged(object sender, EventArgs e)
         {
             int selectedFontSize = int.Parse(cbbFontSizes.SelectedItem.ToString());
-            rtbContent.SelectionFont = new Font(rtbContent.SelectionFont.FontFamily, selectedFontSize);
-        }
 
+            // Lưu vị trí bắt đầu và kết thúc của phần văn bản cần tô đen
+            int start = rtbContent.SelectionStart;
+            int length = rtbContent.SelectionLength;
+
+            
+            rtbContent.SelectionFont = new Font(rtbContent.SelectionFont.FontFamily, selectedFontSize, rtbContent.SelectionFont.Style);
+            
+            // Đặt lại vị trí và độ dài của phần văn bản đã chọn ban đầu
+            rtbContent.SelectionStart = start;
+            rtbContent.SelectionLength = length;
+        }
         private void cbbFonts_SelectedIndexChanged(object sender, EventArgs e)
         {
+            float initialFontSize = rtbContent.Font.Size;
             string selectedFont = cbbFonts.SelectedItem.ToString();
-            Font font = new Font(selectedFont, rtbContent.Font.Size);
-            rtbContent.Font = font;
+            // Lưu vị trí bắt đầu và kết thúc của phần văn bản cần tô đen
+            int start = rtbContent.SelectionStart;
+            int length = rtbContent.SelectionLength;
+
+            rtbContent.SelectionFont = new Font(selectedFont, initialFontSize, rtbContent.Font.Style); ;
+
+            // Đặt lại vị trí và độ dài của phần văn bản đã chọn ban đầu
+            rtbContent.SelectionStart = start;
+            rtbContent.SelectionLength = length;
+            //rtbContent.Font = font;
         }
-
-
-
-
-
         private void btnBold_Click(object sender, EventArgs e)
         {
             // Lưu vị trí bắt đầu và kết thúc của phần văn bản cần tô đen
@@ -216,26 +254,17 @@ namespace MiniWord_HuynhDucHung
             bool isBold = rtbContent.SelectionFont.Bold;
             if (isBold)
             {
-                rtbContent.SelectionFont = new Font(rtbContent.SelectionFont, rtbContent.SelectionFont.Style & ~FontStyle.Bold);
+                rtbContent.SelectionFont = new Font(rtbContent.Font.FontFamily, rtbContent.SelectionFont.Size, rtbContent.SelectionFont.Style & ~FontStyle.Bold);
                 return;
             }
-
             // Tạo một kiểu chữ kết hợp
             FontStyle currentFontStyle = rtbContent.SelectionFont.Style;
-            rtbContent.SelectionFont = new Font(rtbContent.Font, currentFontStyle | FontStyle.Bold);
-
-
-
+            rtbContent.SelectionFont = new Font(rtbContent.Font.FontFamily,rtbContent.SelectionFont.Size, currentFontStyle | FontStyle.Bold);
+            
             // Đặt lại vị trí và độ dài của phần văn bản đã chọn ban đầu
             rtbContent.SelectionStart = start;
             rtbContent.SelectionLength = length;
-
-
-
         }
-
-        
-
         private void btnItalicized_Click(object sender, EventArgs e)
         {
             // Lưu vị trí bắt đầu và kết thúc của phần văn bản cần tô đen
@@ -245,14 +274,14 @@ namespace MiniWord_HuynhDucHung
             bool isItalic = rtbContent.SelectionFont.Italic;
             if (isItalic)
             {
-                rtbContent.SelectionFont = new Font(rtbContent.SelectionFont, rtbContent.SelectionFont.Style & ~FontStyle.Italic);
+                rtbContent.SelectionFont = new Font(rtbContent.Font.FontFamily, rtbContent.SelectionFont.Size, rtbContent.SelectionFont.Style & ~FontStyle.Italic);
                 return;
             }
 
 
             // Tạo một kiểu chữ kết hợp
             FontStyle currentFontStyle = rtbContent.SelectionFont.Style;
-            rtbContent.SelectionFont = new Font(rtbContent.Font, currentFontStyle | FontStyle.Italic);
+            rtbContent.SelectionFont = new Font(rtbContent.Font.FontFamily, rtbContent.SelectionFont.Size, currentFontStyle | FontStyle.Italic);
 
 
            
@@ -264,10 +293,6 @@ namespace MiniWord_HuynhDucHung
 
             
         }
-
-
-  
-
         private void btnUnderline_Click(object sender, EventArgs e)
         {
             // Lưu vị trí bắt đầu và kết thúc của phần văn bản cần tô đen
@@ -277,13 +302,13 @@ namespace MiniWord_HuynhDucHung
             bool isUnderline = rtbContent.SelectionFont.Underline;
             if (isUnderline)
             {
-                rtbContent.SelectionFont = new Font(rtbContent.SelectionFont, rtbContent.SelectionFont.Style & ~FontStyle.Underline);
+                rtbContent.SelectionFont = new Font(rtbContent.Font.FontFamily, rtbContent.SelectionFont.Size, rtbContent.SelectionFont.Style & ~FontStyle.Underline);
                 return;
             }
 
             // Tạo một kiểu chữ kết hợp
             FontStyle currentFontStyle = rtbContent.SelectionFont.Style;
-            rtbContent.SelectionFont = new Font(rtbContent.Font, currentFontStyle | FontStyle.Underline);
+            rtbContent.SelectionFont = new Font(rtbContent.Font.FontFamily, rtbContent.SelectionFont.Size, currentFontStyle | FontStyle.Underline);
 
             // Đặt lại vị trí và độ dài của phần văn bản đã chọn ban đầu
             rtbContent.SelectionStart = start;
@@ -292,12 +317,10 @@ namespace MiniWord_HuynhDucHung
             
             
         }
-
         private void toolNewFile_Click(object sender, EventArgs e)
         {
             NewFile();
         }
-
         private void btnNewFile_Click(object sender, EventArgs e)
         {
             NewFile();
